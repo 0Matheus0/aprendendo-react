3e736c9cb927d1df057a4290e1f9d8932ed41a8b
Matheus Mendes <mendesmatheus50@gmail.com>
2021-06-09 10:46:25 -0300

    useState, aprendendo hooks

--- src/Component/Rota1.jsx
+++ src/Component/Rota1.jsx
@@ -1,16 +1,39 @@
-const Rota1 = () =>{
+import { useState } from "react";
+
+const Rota1 = (props) =>{
 
     //java script 
+    
     //html ou outros
+
+    //useState hook, atualiza os dados em tempo real
+
+
+
+
+    const[count, setCount] = useState(0);
 
 
     return (
         <div>
 
-            <h1>Olá, essa rota 1</h1>
+            <h1>Olá, essa é a rota 1</h1>
+
+            <h2>UseState:{count}</h2>
+
+            <button onClick = {() =>setCount (count-1)}>-1</button> {/* quando clicar diminui o count*/}
+            <button onClick = {() =>setCount (count+1)}>+1</button> {/* quando clicar aumenta o count*/}
+            <br/>
+            <button onClick = {() =>setCount (count-1000)}>-1000</button>
+            <button onClick = {() =>setCount (count+1000)}>+1000</button>
+            <br/>
+            <button onClick = {() =>setCount (count-100)}>-100</button> 
+            <button onClick = {() =>setCount (count+100)}>+100</button>
+
+
 
         </div>
-    )
+    );
 
 
 }
--- src/Component/Rota2.jsx
+++ src/Component/Rota2.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
 
 const Rota2 = () =>{
 
+    const [name, setName] = useState("");
+
+    const clicanobotao = (event) =>{
+        setName(event.target.value)
+
+    }
+    
+    const insert = (escreve) =>{
+        setName(escreve.target.value)
+    }
+
     return (
         <div>
-            <h1>Olá, essa é rota 2</h1>     
+            <h1>Olá, essa é rota 2</h1>
+
+            <input value={name} onChange = {insert}/>
+
+            <br/>
+
+            <button onClick = {clicanobotao}>enviar</button>
+
+            <br/>
+
         </div>
     );
 }
